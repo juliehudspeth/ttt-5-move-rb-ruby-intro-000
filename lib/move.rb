f85051e808905793be7ded786ae897351ae16a7f
Julie <juliejhudspeth@gmail.com>
2018-05-14 18:34:39 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,12 @@
-def input_to_index (user_input)
+
+  def input_to_index (user_input)
   converted_input = user_input.to_i - 1
+end
+
+def move (array, index, value)
+  puts "value", array
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
 end
